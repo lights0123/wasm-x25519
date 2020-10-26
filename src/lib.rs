a04376a68c100d437dd8f2ad780365aabb4023b3
Łukasz Kurowski <crackcomm@gmail.com>
2020-10-26 05:27:15 +0100

    Conversion between ed25519 and x25519

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use curve25519_dalek::edwards::CompressedEdwardsY;
+use curve25519_dalek::montgomery::MontgomeryPoint;
 use js_sys::{Error as JsError, JsString};
 use rand::rngs::OsRng;
 use std::str::from_utf8;
@@ -54,6 +56,12 @@ pub fn base64_decode(data: &str) -> Result<Box<[u8]>, JsValue> {
         .map(Vec::into_boxed_slice)
 }
 
+/// Generates a pair of `x25519` keys.
+///
+/// # Returns
+///
+/// * `keypair`: `x25519` key pair `[private, public]`.
+///
 #[wasm_bindgen]
 pub fn generate_keypair() -> KeyPair {
     let secret = StaticSecret::new(OsRng);
@@ -64,10 +72,66 @@ pub fn generate_keypair() -> KeyPair {
     JsValue::from(ret).into()
 }
 
+/// Derives `x25519` public key from a private key.
+///
+/// # Inputs
+///
+/// * `secret_key`: `x25519` private key.
+///
+/// # Returns
+///
+/// * `public_key`: `x25519` public key.
+///
 #[wasm_bindgen]
 pub fn derive_public(secret_key: &str) -> Result<JsString, JsValue> {
     let mut secret = [0; KEY_SIZE];
     let _ = b64_decode(secret_key.as_bytes(), &mut secret).map_err(|_| JsError::new("Base64"))?;
-    let public = PublicKey::from(secret);
+    let secret = StaticSecret::from(secret);
+    let public = PublicKey::from(&secret);
+    Ok(encode_key(&public.to_bytes()))
+}
+
+/// Converts `ed25519` public key to `x25519` public key.
+///
+/// # Inputs
+///
+/// * `public_key`: `ed25519` public key.
+///
+/// # Returns
+///
+/// * `public_key`: `x25519` public key.
+///
+#[wasm_bindgen]
+pub fn decompress_public(public_key: &str) -> Result<JsString, JsValue> {
+    let mut public = [0; KEY_SIZE];
+    let _ = b64_decode(public_key.as_bytes(), &mut public).map_err(|_| JsError::new("Base64"))?;
+    let compressed = CompressedEdwardsY::from_slice(&public[..]);
+    let public = compressed
+        .decompress()
+        .ok_or_else(|| JsError::new("Base64"))?
+        .to_montgomery();
+    Ok(encode_key(&public.to_bytes()))
+}
+
+/// Converts `x25519` public key to `ed25519` public key.
+///
+/// # Inputs
+///
+/// * `public_key`: `x25519` public key.
+/// * `sign`: a `u8` donating the desired sign of the resulting
+///   `EdwardsPoint`.  `0` denotes positive and `1` negative.
+///
+/// # Returns
+///
+/// * `public_key`: `ed25519` public key.
+///
+#[wasm_bindgen]
+pub fn compress_public(public_key: &str, sign: u8) -> Result<JsString, JsValue> {
+    let mut public = [0; KEY_SIZE];
+    let _ = b64_decode(public_key.as_bytes(), &mut public).map_err(|_| JsError::new("Base64"))?;
+    let public = MontgomeryPoint(public)
+        .to_edwards(sign)
+        .ok_or_else(|| JsError::new("Montgomery Point"))?
+        .compress();
     Ok(encode_key(&public.to_bytes()))
 }
